c9443704505c3369c68eb9f9b9d97f21c1b173c6
kimberlycase91 <kimberlycase91@gmail.com>
2020-03-11 13:16:18 -0700

    added orm file

---
+++ config/orm.js
@@ -0,0 +1,12 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+//select all
+
+//insert one
+
+//update one
+
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
