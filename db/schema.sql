1ce1240649f9b3363ea13ed585442e29ea51538f
kimberlycase91 <kimberlycase91@gmail.com>
2020-03-11 13:12:55 -0700

    ao sql  server

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
 -- Create the burgers_db.
+CREATE DATABASE burgers_db;
 -- Switch to or use the burgers_db.
+USE burgers_db;
 -- Create a burgers table with these fields:
-
-
--- id: an auto incrementing int that serves as the primary key.
-
--- burger_name: a string.
-
--- devoured: a boolean.
+CREATE TABLE burgers (
+id INT auto_increment,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOLEAN,
+PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,4 @@
 -- In this file, write insert queries to populate the burgers table with at least three entries.
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Bacon Avocado Burger", false), ("Chipotle Burger", false), ("Black Bean Burger", false);burgers
