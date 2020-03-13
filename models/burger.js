5d7c292941552bc872e9d2216b4107445e025aeb
kimberlycase91 <kimberlycase91@gmail.com>
2020-03-12 17:28:00 -0700

    set up controllers and models

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,29 @@
-// Inside burger.js, import orm.js into burger.js
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
-
-
-// Export at the end of the burger.js file.
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
